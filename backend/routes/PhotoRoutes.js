9e02c4e71600d0a02087d28dd28226006537ddbb
Gabriel Conceição Nunes <gabrielconceicaonunes@gmail.com>
2024-08-23 10:56:42 -0300

    Implementando a inserção de fotos

--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -2,11 +2,14 @@ const express = require("express")
 const router = express.Router()
 
 // Controller
+const { insertPhoto } = require("../controllers/PhotoController")
 
 // Middlewares
 const { photoInsertValidation } = require("../middlewares/photoValidation")
 const authGuard = require("../middlewares/authGuard")
 const validate = require("../middlewares/handleValidation")
-// Routes
+const { imageUpload } = require("../middlewares/imageUpload")
 
+// Routes
+router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation(), validate, insertPhoto)
 module.exports = router
