c86823df101371ab99f55eff865c83ea8e6be79c
Gabriel Conceição Nunes <gabrielconceicaonunes@gmail.com>
2024-08-26 16:42:05 -0300

    Resgate e atualização de foto

--- backend/middlewares/photoValidation.js
+++ backend/middlewares/photoValidation.js
@@ -19,4 +19,25 @@ const photoInsertValidation = () => {
     ]
 }
 
-module.exports = {photoInsertValidation}
+const photoUpdateValidation = () => {
+    return [
+        body("title")
+        .optional()
+        .isString()
+        .withMessage("O título é obrigatório")
+        .isLength({min: 3})
+        .withMessage("O título precisa ter no mínimo 3 caracteres")
+    ]
+}
+
+const commentValidation = () => {
+    return [
+        body("comment").isString().withMessage("O comentário é obrigatório")
+    ]
+}
+
+module.exports = {
+    photoInsertValidation,
+    photoUpdateValidation,
+    commentValidation
+}
