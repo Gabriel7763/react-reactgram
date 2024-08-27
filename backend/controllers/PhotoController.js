1e3acbbcbf25a8f6b26c69b7dcee691cd998749d
Gabriel Conceição Nunes <gabrielconceicaonunes@gmail.com>
2024-08-27 14:37:55 -0300

    Implementando funcionalidade de buscar fotos

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -176,6 +176,15 @@ const commentPhoto = async (req, res) => {
     })
 }
 
+// Search photos by title
+const searchPhotos = async (req, res) => {
+    const {q} = req.query
+
+    const photos = await Photo.find({title: new RegExp(q, "i")}).exec()
+
+    res.status(200).json(photos)
+}
+
 module.exports = {
     insertPhoto, 
     deletePhoto, 
@@ -184,5 +193,6 @@ module.exports = {
     getPhotoById, 
     updatePhoto, 
     likePhoto, 
-    commentPhoto
+    commentPhoto,
+    searchPhotos
 }
--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -2,10 +2,24 @@ const express = require("express")
 const router = express.Router()
 
 // Controller
-const { insertPhoto, deletePhoto, getAllPhotos, getUserPhotos, getPhotoById, updatePhoto, likePhoto, commentPhoto } = require("../controllers/PhotoController")
+const { 
+    insertPhoto,
+    deletePhoto,
+    getAllPhotos,
+    getUserPhotos,
+    getPhotoById,
+    updatePhoto,
+    likePhoto,
+    commentPhoto,
+    searchPhotos
+} = require("../controllers/PhotoController")
 
 // Middlewares
-const { photoInsertValidation, photoUpdateValidation, commentValidation } = require("../middlewares/photoValidation")
+const { 
+    photoInsertValidation, 
+    photoUpdateValidation, 
+    commentValidation
+} = require("../middlewares/photoValidation")
 const authGuard = require("../middlewares/authGuard")
 const validate = require("../middlewares/handleValidation")
 const { imageUpload } = require("../middlewares/imageUpload")
@@ -15,6 +29,7 @@ router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation()
 router.delete("/:id", authGuard, deletePhoto)
 router.get("/", authGuard, getAllPhotos)
 router.get("/user/:id", authGuard, getUserPhotos)
+router.get("/search", authGuard, searchPhotos)
 router.get("/:id", authGuard, getPhotoById)
 router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto)
 router.put("/like/:id", authGuard, likePhoto)
