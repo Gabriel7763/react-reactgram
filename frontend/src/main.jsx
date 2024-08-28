1c916a6fafec79b339500fe08ad4112f6f6489ae
Gabriel Conceição Nunes <gabrielconceicaonunes@gmail.com>
2024-08-28 20:48:07 -0300

    Implementando Redux e corrigindo erros

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Redux
+import {Provider} from 'react-redux'
+import {store} from './store.js'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
