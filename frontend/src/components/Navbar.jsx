91361fda75ad4db0bc9a168f5eecda1b444c870f
Gabriel Conceição Nunes <gabrielconceicaonunes@gmail.com>
2024-08-27 21:10:31 -0300

    Implamentado o formulario de cadastro

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,30 +10,35 @@ import {
 } from 'react-icons/bs'
 
 const Navbar = () => {
-    return <nav id="nav">
-        <Link to="/">ReactGram</Link>
-        <form>
-            <BsSearch />
-            <input type="text" placeholder="Pesquisar" />
-            <ul id="nav-links">
-                <li>
-                    <NavLink to="/">
-                        <BsHouseDoorFill />
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/login">
-                        Entrar
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/register">
-                        Cadastrar
-                    </NavLink>
-                </li>
-            </ul>
-        </form>
-    </nav>
+    return (
+        <nav id="nav">
+            <Link to="/">
+                <h2>ReactGram</h2>
+            </Link>
+            <form id="search-form">
+                <BsSearch />
+                <input type="text" placeholder="Pesquisar" />
+                <ul id="nav-links">
+                    <li>
+                        <NavLink to="/">
+                            <BsHouseDoorFill />
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/login">
+                            Entrar
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/register">
+                            Cadastrar
+                        </NavLink>
+                    </li>
+                </ul>
+            </form>
+        </nav>
+    )
+    
 }
 
 export default Navbar
